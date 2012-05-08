523a48ba1904649246900d1c2f397a7daa3936fc
Dale Ragan <dale@ragan.io>
2012-05-08 01:50:42 -0400

    Creating project for acceptance tests.

---
+++ src/Mulder.Acceptance.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("Mulder.Acceptance.Tests")]
+[assembly: AssemblyDescription("Mulder Acceptance Test Suite")]
