3da3d86ba01e6971e04b962b776fd896a62e6920
Dale Ragan <dale@ragan.io>
2012-05-09 09:52:26 -0400

    Wiring up Cli with our entry point.

--- src/Mulder.Cli/Program.cs
+++ src/Mulder.Cli/Program.cs
@@ -1,12 +1,42 @@
 using System;
+using System.Collections.Generic;
+
+using Mulder.Base;
+using Mulder.Base.Commands;
+using Mulder.Base.DataSources;
+using Mulder.Base.IO;
+using Mulder.Base.Logging;
 
 namespace Mulder.Cli
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static int Main(string[] args)
 		{
-			Console.WriteLine("The Truth Is Out There");
+			ExitCode exitCode;
+			ILog log = new Log(Console.Out, new LogLevel[] { LogLevel.Info, LogLevel.Error });
+			
+			try {
+				var entryPoint = CreateEntryPoint(log);
+				exitCode = entryPoint.Run(args);
+			} catch (Exception e) {
+				log.ErrorMessage(e);
+				exitCode = ExitCode.Error;
+			}
+			
+			return (int)exitCode;
+		}
+		
+		static EntryPoint CreateEntryPoint(ILog log)
+		{
+			var createCommands = new Dictionary<string, ICommand>();
+			var fileSystem = new FileSystem();
+			createCommands.Add("site", new CreateSiteCommand(log, fileSystem, new FileSystemUnified(log, fileSystem)));
+			
+			var commands = new Dictionary<string, ICommand>();
+			commands.Add("create", new CreateCommand(log, createCommands));
+			
+			return new EntryPoint(log, commands);
 		}
 	}
 }
