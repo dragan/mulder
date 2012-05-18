cc87e74780a7acb71d93cae50cc501f5ed020161
Dale Ragan <dale@ragan.io>
2012-05-18 18:59:33 -0400

    Updating Sake to version 0.1.3.

--- sake.cmd
+++ sake.cmd
@@ -2,5 +2,5 @@
 cd %~dp0
 
 set EnableNuGetPackageRestore=true
-".nuget\NuGet.exe" install Sake -Version 0.1.0-alpha-4 -o packages
-"packages\Sake.0.1.0-alpha-4\tools\Sake.exe" -f Sakefile.shade -I packages\Sake.0.1.0-alpha-4\tools\Shared %*
+".nuget\NuGet.exe" install Sake -Version 0.1.3 -o packages
+"packages\Sake.0.1.3\tools\Sake.exe" -f Sakefile.shade -I packages\Sake.0.1.3\tools\Shared %*
