ab5b44d5080c90176234e83628b37b2e5460137b
Dale Ragan <dale@ragan.io>
2012-09-21 09:07:46 -0400

    Supporting includes in Razor filter.

--- src/Mulder.Base/Filters/RazorFilter.cs
+++ src/Mulder.Base/Filters/RazorFilter.cs
@@ -1,6 +1,10 @@
+using Microsoft.CSharp.RuntimeBinder;
 using System;
+using System.IO;
 
 using RazorEngine;
+using RazorEngine.Configuration;
+using RazorEngine.Templating;
 
 namespace Mulder.Base.Filters
 {
@@ -12,7 +16,59 @@ namespace Mulder.Base.Filters
 
 		public string Execute(string source, dynamic model)
 		{
+			string includesPath = GetIncludesPath(model);
+
+			var serviceConfig = new TemplateServiceConfiguration {
+				Resolver = new IncludesResolver(includesPath)
+			};
+
+			Razor.SetTemplateService(new TemplateService(serviceConfig));
+
 			return Razor.Parse(source, model);
+		}
+
+		string GetIncludesPath(dynamic model)
+		{
+			string includesPath = string.Empty;
+
+			try {
+				var layoutMeta = model.Layout;
+				
+				if (layoutMeta.ContainsKey("includes_path"))
+					includesPath = layoutMeta["includes_path"].ToString();
+			}
+			catch (RuntimeBinderException e) {
+				// Just ignore the exception if it doesn't exist
+			}
+			
+			return includesPath;
+		}
+	}
+
+	class IncludesResolver : ITemplateResolver
+	{
+		string root;
+		
+		public IncludesResolver(string root)
+		{
+			this.root = root;
+		}
+		
+		public string Resolve(string name)
+		{
+			string includesPath = Path.Combine(root, name);
+			bool exists = false;
+
+			foreach (var ext in new[] { ".cshtml", ".html", ".htm" }) {
+				var testPath = String.Concat(includesPath, ext);
+				exists = File.Exists(testPath);
+				if (exists) {
+					includesPath = testPath;
+					break;
+				}
+			}
+
+			return exists ? File.ReadAllText(includesPath) : String.Empty;
 		}
 	}
 }
